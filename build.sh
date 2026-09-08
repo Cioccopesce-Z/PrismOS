6fd4985081bdd7f83f75253f2da15ebfe739b64d
MrOld <cioccopesce43@gmail.com>
2026-09-08 22:06:07 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-export PREFIX="$HOME/opt/cross"
-export TARGET=i686-elf
-export PATH="$PREFIX/bin:$PATH"
-clear && make clean && echo "hehehe you want see this" && make all && qemu-system-i386 -hda bin/os.bin -m 8G
-
