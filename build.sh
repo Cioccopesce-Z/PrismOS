9d8d3b4f28883b037d18c97f7079c4ff7977d4d8
MrOld <cioccopesce43@gmail.com>
2026-09-08 22:32:57 +0200

    limited the necessasiry of ram at 1.5mb

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
-clear && make clean && echo "hehehe you want see this" && make all && qemu-system-i386 -hda bin/os.bin -m 8G
-
+clear && make clean && echo "hehehe you want see this" && make all && qemu-system-i386 -hda bin/os.bin -m 1.5M
